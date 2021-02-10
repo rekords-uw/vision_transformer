8d97004e009aee1bc9820ced9e19fd8b2633f1e5
zliu676 <zliu676@cosmos0004.chtc.wisc.edu>
2021-02-09 22:39:33 -0600

    add imagenet

--- vit_jax/input_pipeline.py
+++ vit_jax/input_pipeline.py
@@ -14,6 +14,7 @@
 
 import flax
 import jax
+import os
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
@@ -66,9 +67,145 @@ def get_dataset_info(dataset, split):
   num_examples = data_builder.info.splits[split].num_examples
   num_classes = data_builder.info.features['label'].num_classes
   return {
-      'num_examples': num_examples,
-      'num_classes': num_classes
+    'num_examples': num_examples,
+    'num_classes': num_classes
   }
+
+def get_filenames(is_training, data_dir):
+  if is_training:
+    return [os.path.join(data_dir, 'train-%05d-of-01024' % i) for i in range(NUM_TRAIN_FILES)]
+  else:
+    return [os.path.join(data_dir, 'val-%05d-of-00128' % i) for i in range(NUM_TEST_FILES)]
+
+def parse_example_proto(example_serialized):
+  """
+  Parses an Example proto containing an image.
+
+  :param example_serialized: scalar Tensor tf.string
+  :return:
+      image_buffer: Tensor tf.string with contents of a JPEG file
+      label: Tensor tf.float32 containing the label
+  """
+  feature_map = {'image/encoded': tf.io.FixedLenFeature([], dtype=tf.string, default_value=''),
+                 'image/class/label': tf.io.FixedLenFeature([], dtype=tf.int64, default_value=-1),}
+
+  features = tf.io.parse_single_example(serialized=example_serialized, features=feature_map)
+
+  # want labels [0, 999] instead of [1, 1000]
+  one_hot = True
+  if one_hot:
+    # one hot encoding without label smoothing
+    smoothing = 0.1
+    label = tf.one_hot(features['image/class/label'] - 1, 1000, 
+                       #on_value=1-smoothing, off_value=smoothing/(1000-1),
+                       dtype=tf.float32)
+  else:
+    label = tf.cast(features['image/class/label'] - 1, dtype=tf.float32)
+
+  return features['image/encoded'], label
+
+def preprocess_image(image_buffer, is_training, inception_crop, resize_size, crop_size):
+  NUM_CHANNELS = 3
+  image = tf.io.decode_jpeg(image_buffer, channels=NUM_CHANNELS)
+  im = tf.cast(image, tf.float32)
+
+  if is_training:
+    if inception_crop:
+      channels = NUM_CHANNELS
+      begin, size, _ = tf.image.sample_distorted_bounding_box(
+          tf.shape(im),
+          tf.zeros([0, 0, 4], tf.float32),
+          area_range=(0.05, 1.0),
+          min_object_covered=0,  # Don't enforce a minimum area.
+          use_image_if_no_bounding_boxes=True)
+      im = tf.slice(im, begin, size)
+      # Unfortunately, the above operation loses the depth-dimension. So we
+      # need to restore it the manual way.
+      im.set_shape([None, None, channels])
+      im = tf.image.resize(im, [crop_size, crop_size])
+    else:
+      im = tf.image.resize(im, [resize_size, resize_size])
+      im = tf.image.random_crop(im, [crop_size, crop_size, 3])
+      im = tf.image.flip_left_right(im)
+  else:
+    # usage of crop_size here is intentional
+    im = tf.image.resize(im, [crop_size, crop_size])
+  im = (im - 127.5) / 127.5
+  return im
+
+def parse_record(raw_record, is_training, inception_crop, resize_size, crop_size):
+  """
+  Parse a record containing an image. Record is parsed into a label and image, and the image is passed through
+  preprocessing steps.
+
+  :param raw_record:
+  :param is_training:
+  :param dtype:
+  :param center_crops_for_train
+  :param single_scale
+  :param color_aug
+  :param standardize_train
+  :param resnet_preprocessing
+  :return:
+  """
+  image_buffer, label = parse_example_proto(raw_record)
+
+  image = preprocess_image(image_buffer, is_training=is_training, inception_crop=inception_crop, 
+                           resize_size=resize_size, crop_size=crop_size)
+
+  return {'image': image, 'label': label}
+
+def get_data_imagenet(base_data_dir,
+                      mode,
+                      repeats,
+                      batch_size,
+                      shuffle_buffer=MAX_IN_MEMORY,
+                      inception_crop=True):
+  preset = DATASET_PRESETS.get('imagenet2012')
+  resize_size = preset['resize']
+  crop_size = preset['crop']
+
+  data_dir = 'train' if mode == 'train' else 'val'
+  filenames = get_filenames(mode == 'train', os.path.join(base_data_dir, data_dir))
+  dataset = tf.data.Dataset.from_tensor_slices(filenames)
+
+  NUM_TRAIN_FILES = 1024
+  if mode == 'train':
+    # Shuffle the input files
+    dataset = dataset.shuffle(buffer_size=NUM_TRAIN_FILES)
+    dataset = dataset.interleave(lambda x: tf.data.TFRecordDataset(x, buffer_size=None, num_parallel_reads=None),
+                                 num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    # Prefetch a batch at a time to smooth out the time taken for processing.
+    dataset = dataset.prefetch(buffer_size=batch_size)
+
+  if is_training:
+    # Shuffles records before repeating to respect epoch boundaries. Show every element of one epoch before next.
+    dataset = dataset.shuffle(buffer_size=shuffle_buffer, reshuffle_each_iteration=True)
+
+  # Repeats the dataset for the number of epochs to train.
+  dataset = dataset.repeat(repeats)
+  # Parses the raw records into images and labels.
+  dataset = dataset.map(lambda value: parse_record(value, mode=='train', inception_crop=inception_crop, 
+                                                   resize_size=resize_size, crop_size=crop_size),
+                        num_parallel_calls=tf.data.experimental.AUTOTUNE)
+  
+  dataset = dataset.batch(batch_size, drop_remainder=True)
+
+  num_devices = jax.local_device_count()
+
+  def _shard(data):
+    data['image'] = tf.reshape(data['image'],
+                               [num_devices, -1, crop_size, crop_size, 3])
+    data['label'] = tf.reshape(data['label'],
+                               [num_devices, -1, 1000])
+    return data
+
+  if num_devices is not None:
+    dataset = dataset.map(_shard, tf.data.experimental.AUTOTUNE)
+
+  return dataset.prefetch(1)
+
+
 
 
 def get_data(*,
---
+++ vit_jax/loadImageNet.py
@@ -0,0 +1,125 @@
+import functools
+import glob
+import os
+import time
+
+from clu import metric_writers
+
+import numpy as np
+
+import jax
+import jax.numpy as jnp
+
+import flax
+import flax.optim as optim
+import flax.jax_utils as flax_utils
+
+import tensorflow as tf
+
+from vit_jax import checkpoint
+from vit_jax import flags
+from vit_jax import hyper
+from vit_jax import logging
+from vit_jax import input_pipeline
+from vit_jax import models
+from vit_jax import momentum_clip
+
+def extract_keys(dict_in, depth=0):
+  for key, value in dict_in.items():
+    for i in range(depth):
+      print(' ', end='')
+    print('-', end='')
+    print(key)
+    if isinstance(value, dict): # If value itself is dictionary
+      extract_keys(value, depth+1)
+    else:
+      for i in range(depth):
+        print(' ', end='')
+      print(value.shape)
+            
+def main(args):
+  attn_record_layer = 11
+  logdir = os.path.join(args.logdir, args.name)
+  logger = logging.setup_logger(logdir)
+  logger.info(args)
+
+  logger.info(f'Available devices: {jax.devices()}')
+
+  # Setup input pipeline
+
+  ds_train = input_pipeline.get_data_imagenet(
+      base_data_dir='imagenet_dir',
+      mode='train',
+      repeats=None,
+      batch_size=args.batch)
+  batch = next(iter(ds_train))
+  logger.info(ds_train)
+  ds_test = input_pipeline.get_datai_imagenet(
+      base_data_dir='imagenet_dir',
+      mode='test',
+      repeats=1,
+      batch_size=args.batch_eval)
+  logger.info(ds_test)
+
+  # Build VisionTransformer architecture
+  model = models.KNOWN_MODELS[args.model]
+  VisionTransformer = model.partial(num_classes=1000, 
+      attn_record_layer=attn_record_layer)
+  _, params = VisionTransformer.init_by_shape(
+      jax.random.PRNGKey(0),
+      # Discard the "num_local_devices" dimension for initialization.
+      [(batch['image'].shape[1:], batch['image'].dtype.name)])
+
+  pretrained_path = os.path.join(args.vit_pretrained_dir, f'{args.model}.npz')
+  params = checkpoint.load_pretrained(
+      pretrained_path=pretrained_path,
+      init_params=params,
+      model_config=models.CONFIGS[args.model],
+      logger=logger)
+  # params = checkpoint.load(pretrained_path)
+  # params['pre_logits'] = {}
+  #extract_keys(params)
+  vit_fn_repl = jax.pmap(VisionTransformer.call)
+
+  opt = momentum_clip.Optimizer(
+      dtype=args.optim_dtype, grad_norm_clip=args.grad_norm_clip).create(params)
+  opt_repl = flax_utils.replicate(opt)
+
+  del opt
+  del params
+  print('***********************')
+
+  acc = []
+
+  first_batch = True
+  get_acc = False
+  for batch in input_pipeline.prefetch(ds_test, args.prefetch):
+    output, attention_matrix = vit_fn_repl(opt_repl.target, batch['image'])
+    if get_acc:
+      batch_acc = np.mean(
+        (np.argmax(output, axis=2) == np.argmax(batch['label'], axis=2)).ravel()
+        )
+      acc.append(batch_acc)
+    if first_batch:
+      print(batch['image'].shape)
+      print(output.shape)
+      print(attention_matrix.shape)
+      jnp.save('batch_image_%s.npy' % args.dataset, batch['image'][:,:10,:])
+      jnp.save('batch_attn_layer%d_%s.npy' % (attn_record_layer, args.dataset), attention_matrix[:,:10,:])
+      jnp.save('batch_label_%s.npy' % args.dataset, batch['label'][:,:10,:])
+      first_batch = False
+
+  if get_acc:
+    acc_test = np.mean(acc)
+    logger.info(f'Test accuracy: {acc_test:0.5f}')
+    print(acc_test)
+
+if __name__ == '__main__':
+  # Make sure tf does not allocate gpu memory.
+  tf.config.experimental.set_visible_devices([], 'GPU')
+
+  parser = flags.argparser(models.KNOWN_MODELS.keys(),
+                           input_pipeline.DATASET_PRESETS.keys())
+
+  main(parser.parse_args())
+
